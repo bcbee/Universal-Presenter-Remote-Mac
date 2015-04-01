5beb1ac54bb81950ddee00ccd4a5d3790b6b57a6
Brendan Boyle <brendancboyle@me.com>
2015-04-01 13:19:05 -0400

    Updated for distribution 1.3

--- UPR Controller Mac/UPR Controller Mac/AppDelegate.swift
+++ UPR Controller Mac/UPR Controller Mac/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
         // Insert code here to initialize your application
-        NSApplication.sharedApplication().registerForRemoteNotificationTypes(.Alert)
+        //NSApplication.sharedApplication().registerForRemoteNotificationTypes(.Alert)
         
     }
 
